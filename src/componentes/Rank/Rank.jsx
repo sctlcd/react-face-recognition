86f61e9b87c7a496cf1206442b8269a91de36fd0
sctlcd <lucinda.souchet@gmail.com>
2025-02-10 19:43:09 +0100

    added Rank component

---
+++ src/componentes/Rank/Rank.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Rank() {
+  return (
+    <>
+      <div className='white'>
+        <span className='f2'>{'Mambo'}</span>
+        <span className='f3'>{', your current rank is...'}</span>
+      </div>
+      <div className='white f2 mb5'>
+        {'Number 5'}
+      </div>
+    </>
+  )
+}
+
